b59aca4133c02d13a0aee23d1787b22bed477c4c
Zoltán Galáž <xgalaz00@gmail.com>
2019-03-29 13:05:17 +0100

    Added essential documentation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Data Science Helpers',
+    version=0.1,
+    description='Useful data-science helpers (visualizations, utility functions, etc.',
+    long_description=open('README.md').read(),
+    author='Zoltán Galáž',
+    author_email='xgalaz00@gmail.com',
+    packages=['helpers'],
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+    ]
+)
